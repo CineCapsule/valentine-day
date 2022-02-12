1de241577974f47cc96e7c27eb2bdbfa53871854
Bertrand Juglas <b@juglas.name>
2022-02-12 22:29:43 +0100

    fix: add missing link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Header(props) {
-    return (
-        <header className="App-header">
-        <nav className="navbar fixed-top navbar-dark bg-dark">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              CC Valentine's Day DApp🍿
-            </a>
-          </div>
-        </nav>
-      </header>
-
-    );
+  return (
+    <header className="App-header">
+      <nav className="navbar fixed-top navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="https://cinecapsule.cc/">
+            CC Valentine's Day DApp🍿
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
 }
