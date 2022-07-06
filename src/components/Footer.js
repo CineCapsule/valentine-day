ff2600c3fc9f837c13d0479d36fe763c869d1f7c
techcine <106427822+techcine@users.noreply.github.com>
2022-07-06 17:27:37 +0200

    fix: css and add text (#4)
    Co-authored-by: marjorie <marjorie.ngoupende@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(props) {
   return (
     <div className="footer">
-      <p>Made with ❤️ by @Team cineCapsule dev.</p>
+      <p>Made with ❤️ by CineCapsule Team 💪</p>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
       <nav className="navbar fixed-top navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="https://cupid.cinecapsule.cc/">
-            CC Valentine's Day DApp🍿
+            CineCapsule Valentine's Day DApp🍿
           </a>
           <a className="navbar-brand" href="https://cinecapsule.cc/">
             About🍿
